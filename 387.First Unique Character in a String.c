5f4fcf696543d5f7cbd2ae7b391c88d796a1b72f
Anushka_Trivedhi <anushkatrivedhi23@gmail.com>
2025-03-07 22:33:07 +0530

    Create 387.First Unique Character in a String.c

---
+++ 387.First Unique Character in a String.c
@@ -0,0 +1,14 @@
+int firstUniqChar(char* s) {
+
+    int freq[256] = {0}; 
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i]]++;
+    }
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[s[i]] == 1) {
+            return i; 
+        }
+    }
+
+    return -1;  
+}
